baace68f096ed957be0d06881fb544244c471302
Jada Desormeaux <JadaE789@gmail.com>
2021-03-17 20:51:25 -0400

    updated and finished comments and javascript

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,16 +1,55 @@
 //Current Date
-var today = moment();
+let today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 $(document).ready(function () {
-    // saveBtn click listener 
+    // Save button clicker 
     $(".saveButton").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
 
-        // Save text in local storage
+        // Saving all information enetered in local storage
         localStorage.setItem(time, text);
     })
 })
 
+function timeTracker() {
+    //Current number of hours.
+    let currentTime  = moment().hour();
+
+    // Looping over the time blocks
+    $(".time-block").each(function () {
+        let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+        // Checks time and changes background color based on current time
+        if (timeBlock < currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (timeBlock === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
+// Pull any information entered in from local storage
+$("#nineAM .description").val(localStorage.getItem("nineAM"));
+$("#tenAM .description").val(localStorage.getItem("tenAM"));
+$("#elevenAM .description").val(localStorage.getItem("elevenAM"));
+$("#twelvePM .description").val(localStorage.getItem("twelvePM"));
+$("#onePM .description").val(localStorage.getItem("onePM"));
+$("#twoPM .description").val(localStorage.getItem("twoPM"));
+$("#threePM .description").val(localStorage.getItem("threePM"));
+$("#fourPM .description").val(localStorage.getItem("fourPM"));
+$("#fivePM .description").val(localStorage.getItem("fivePM"));
+
+timeTracker();
